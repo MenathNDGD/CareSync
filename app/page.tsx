c37177ae9d2152e11a561970b4f0a86a794e9c06
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-15 21:24:04 +0530

    modified the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,42 @@
-import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl text-white underline">Home</h1>
-      <Button>Click me</Button>
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP Verification | PasskeyModal */}
+
+      <section className="container my-auto remove-scrollbar">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src={"/assets/icons/logo-full.png"}
+            height={1000}
+            width={1000}
+            alt="CareSync"
+            className="h-10 mb-12 w-fit"
+          />
+
+          <PatientForm />
+
+          <div className="flex justify-between mt-20 text-14-regular">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CareSync
+            </p>
+            <Link href={"/?admin=true"} className="text-green-500 ">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <Image
+        src={"/assets/images/onboarding-img.png"}
+        height={1000}
+        width={1000}
+        alt="Patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
