3934442abca84b7830dd5c1f053a39ca6e9a3ab4
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-17 11:44:23 +0530

    modified the patient actions library

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -52,6 +52,24 @@ export const getUser = async (userId: string) => {
   }
 };
 
+// INFO: GET PATIENT DETAILS
+export const getPatient = async (userId: string) => {
+  try {
+    const patients = await databases.listDocuments(
+      DATABASE_ID!,
+      PATIENT_COLLECTION_ID!,
+      [Query.equal("userId", userId)]
+    );
+
+    return parseStringify(patients.documents[0]);
+  } catch (error) {
+    console.error(
+      "An error occurred while retrieving the patient details:",
+      error
+    );
+  }
+};
+
 // INFO: REGISTER A NEW PATIENT
 export const registerPatient = async ({
   identificationDocument,
