4a896bb42c7013a1ec4cf087004bfe66a60df030
Menath Nadungoda <nadungmenath1@gmail.com>
2024-09-17 15:07:37 +0530

    modified the submit button component

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Button } from "./ui/button";
 import Image from "next/image";
+
+import { Button } from "./ui/button";
 
 interface ButtonProps {
   isLoading: boolean;
@@ -18,8 +18,8 @@ const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
       {isLoading ? (
         <div className="flex items-center gap-4">
           <Image
-            src={"/assets/icons/loader.svg"}
-            alt="Loading Icon"
+            src="/assets/icons/loader.svg"
+            alt="loader"
             width={24}
             height={24}
             className="animate-spin"
